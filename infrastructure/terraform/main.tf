306ef964db9abab85cb45897305a6804a7113cba
Borucinski, Michal <mborucinski@kpmg.pl>
2021-03-07 23:23:26 +0100

    Add app service to terraform

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -6,16 +6,38 @@ terraform {
       version = ">= 2.26"
     }
   }
-  
-  backend "azurerm" {
+
+  backend "azurem" {
   }
 }
 
-provider "azurerm" {
+provider "azurem" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "test" {
   name     = "wedding-eu-test-rg"
   location = "westeurope"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "wedding-web-test-asp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  app_settings = {
+    "ASPNETCORE_ENVIRONMENT" = "Test"
+  }
 }
